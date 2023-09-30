adc9258098eb90d7bfec1996b7783e06c0fc7744
Gabriel Camilo <74691430+GabrielCamiloOliveira@users.noreply.github.com>
2023-09-30 15:22:36 -0300

    Create HelloWorld.java

---
+++ hello/HelloWorld.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+     public static void main(String[] args) {
+        String mensagem = "Olá mundo!";
+         
+        JOptionPane.showMessageDialog(null, mensagem);
+      
+        System.out.println(mensagem);
+    }
+}
