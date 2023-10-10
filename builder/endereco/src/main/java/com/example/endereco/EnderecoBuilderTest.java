ab2774c825d12905e78edb11f743c1a2a9a8c931
Gabriel Camilo <74691430+GabrielCamiloOliveira@users.noreply.github.com>
2023-10-10 20:19:55 -0300

    Update EnderecoBuilderTest.java

--- builder/endereco/src/main/java/com/example/endereco/EnderecoBuilderTest.java
+++ builder/endereco/src/main/java/com/example/endereco/EnderecoBuilderTest.java
@@ -19,14 +19,14 @@ public class EnderecoBuilderTest {
                 .build();
 
         assertNotNull(teste1);
-        assertEquals("Praça Júlio Prestes", teste1.getRua());
-        assertEquals("Campos Elíseos", teste1.getSetor());
-        assertEquals(12180200, teste1.getCep());
-        assertEquals(16, teste1.getNumero());
-        assertEquals("São Paulo", teste1.getCidade());
-        assertEquals("São Paulo", teste1.getEstado());
-        assertEquals("1", teste1.getBloco());
-        assertEquals("15", teste1.getQuadra());
+        assertEquals("Avenida T-4", teste1.getRua());
+        assertEquals("Setor Bueno", teste1.getSetor());
+        assertEquals(74230022, teste1.getCep());
+        assertEquals(10, teste1.getNumero());
+        assertEquals("Goiânia", teste1.getCidade());
+        assertEquals("Goiás", teste1.getEstado());
+        assertEquals("3", teste1.getBloco());
+        assertEquals("12", teste1.getQuadra());
 
         EnderecoApplication teste2 = EnderecoApplication.builder()
                 .rua("Praça Júlio Prestes")
@@ -39,15 +39,14 @@ public class EnderecoBuilderTest {
                 .quadra("15")
                 .build();
 
-        assertNotNull(teste2);
-        assertEquals("Avenida T-4", teste2.getRua());
-        assertEquals("Setor Bueno", teste2.getSetor());
-        assertEquals(74230022, teste2.getCep());
-        assertEquals(10, teste2.getNumero());
-        assertEquals("Goiânia", teste2.getCidade());
-        assertEquals("Goiás", teste2.getEstado());
-        assertEquals("3", teste2.getBloco());
-        assertEquals("12", teste2.getQuadra());
+         assertEquals("Praça Júlio Prestes", teste2.getRua());
+        assertEquals("Campos Elíseos", teste2.getSetor());
+        assertEquals(12180200, teste2.getCep());
+        assertEquals(16, teste2.getNumero());
+        assertEquals("São Paulo", teste2.getCidade());
+        assertEquals("São Paulo", teste2.getEstado());
+        assertEquals("1", teste2.getBloco());
+        assertEquals("15", teste2.getQuadra());
 
         EnderecoApplication teste3 = EnderecoApplication.builder() //teste para verificar se o created é inicializado diferente de 0
         .rua("Rua Teste")
